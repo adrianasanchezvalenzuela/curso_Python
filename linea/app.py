544d2c961be8dcbda1a7c62f980d93ed288b4113
adrianasanchezvalenzuela <alysan1505@gmail.com>
2025-01-28 10:53:51 -0700

    Agregué grafica_linea() a funciones.py

--- linea/app.py
+++ linea/app.py
@@ -2,23 +2,19 @@
 import funciones
 import argparse
 
+
 def main(m:float, b:float):
     '''
     Función principal que calcula las coordenadas de una línea recta
     Recibimos m y b
     Regresa: nada'''
-    #m = 2.0
-    #b = 3.0
-    #X = [x for x in range(1,11)]
-    #Y = [funciones.calcular_y(x,m,b) for x in X]
-    #print("Enteros:")
-    #coordenadas_enteros = list(zip(X,Y))
-    #print (coordenadas_enteros)
-    XF = [x/10.0 for x in range(10,110,5)]
-    YF = [funciones.calcular_y(x,m,b) for x in XF]
-    coordenadas_flotantes = list(zip(XF,YF))
+    X = [x/10.0 for x in range(10,110,5)]
+    Y = [funciones.calcular_y(x,m,b) for x in X]
+    coordenadas_flotantes = list(zip(X,Y))
     print("Flotantes:")
     print(coordenadas_flotantes)
+    funciones.grafica_linea(X,Y,m,b)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
--- linea/funciones.py
+++ linea/funciones.py
@@ -1,4 +1,6 @@
 #archivo con todas las funciones necesarias para la aplicación "línea"
+import matplotlib.pyplot as plt
+
 def calcular_y(x: float,m: float,b:float)->float:
     '''
     Calcula el valor de y en una línea recta
@@ -7,6 +9,19 @@ def calcular_y(x: float,m: float,b:float)->float:
     b: intersección de y
     '''
     return (m*x)+b
+
+def grafica_linea(X:list, Y:list, m:float, b:float):
+     '''
+    Gráfica de una línea recta
+    x: lista de valores de x
+    y: lista de valores de y
+    Regresa: nada
+    '''
+     plt.plot(X,Y)
+     plt.title(f'Línea con pendiente {m} y ordenada al origen {b}')
+     plt.xlabel('X')
+     plt.ylabel('Y')
+     plt.show()
 
 def test_linea():
     '''
