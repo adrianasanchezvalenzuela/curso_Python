f3cf7b2b7bbc46df3f29c3c2dd274c000e9b3309
adrianasanchezvalenzuela <alysan1505@gmail.com>
2025-02-04 10:37:40 -0700

    Tenemos las combinaciones

--- Juego_gato/tablero.py
+++ Juego_gato/tablero.py
@@ -41,6 +41,22 @@ def usuario(simbolos:dict):
         else:
             print('Numero incorrecto')
 
+def juego(simbolos:dict):
+    '''
+    Juego del gato
+    '''
+lista_combinaciones = [
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '9'],
+    ['1', '4', '7'],
+    ['2', '5', '8'],
+    ['3', '6', '9'],
+    ['1', '5', '9'],
+    ['3', '5', '7']
+]
+
+
 
 if __name__ == '__main__':
     numeros= [str(x) for x in range(1,10)]
